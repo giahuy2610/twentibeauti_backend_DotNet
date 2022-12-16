9bf300debbe05395eea9bfa7df8216c76afeb7a2
giahuy2610 <giahuytrinh.26102002@gmail.com>
2022-12-16 22:04:31 +0700

    push

--- TwentiBeauti_BackEnd_DotNet/Controllers/CouponController.cs
+++ TwentiBeauti_BackEnd_DotNet/Controllers/CouponController.cs
@@ -9,7 +9,7 @@ namespace TwentiBeauti_BackEnd_DotNet.Controllers
 {
 
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/coupon")]
     public class CouponController : ControllerBase
     {
         private readonly Context dbContextCoupon;
@@ -18,16 +18,40 @@ namespace TwentiBeauti_BackEnd_DotNet.Controllers
             this.dbContextCoupon = dbContextCoupon;
         }
 
-       
+        [HttpGet("index")]
+        public async Task<IActionResult> index()
+        {
+            return (new JsonResult(await dbContextCoupon.Coupon.ToListAsync()));
+        }
+
+        [HttpGet("available")]
+        public async Task<IActionResult> indexAvailable()
+        {
+            return (Ok(await dbContextCoupon.Coupon.Where(c => c.StartOn <= DateTime.Now && c.EndOn >= DateTime.Now && c.Stock > 0 && c.IsDeleted == false).ToListAsync()));
+        }
+
+        [HttpGet("show/{IDCoupon:int}")]
+        public async Task<IActionResult> show(int IDCoupon)
+        {
+            return Ok(await dbContextCoupon.Coupon.FindAsync(IDCoupon));
+        }
+
+        [HttpGet("check-available/{IDCoupon:int}")]
+        public async Task<IActionResult> checkAvailable(int IDCoupon)
+        {
+            var c = await dbContextCoupon.Coupon.FindAsync(IDCoupon);
+            if (c == null) return NotFound();
+            else if (c.StartOn <= DateTime.Now && c.EndOn >= DateTime.Now && c.Stock > 0 && c.IsDeleted == false)
+            return Ok(c);
+            else return BadRequest();
+        }
+
         [HttpPost]
         [Route("post/{CodeCoupon}")]
         public async Task<IActionResult> GetCoupon([FromRoute] string CodeCoupon)
         {
 
             return Ok(this.dbContextCoupon.Coupon.Where(coupon => coupon.CodeCoupon == CodeCoupon).First().IDCoupon);
-
-            
-            //return Context.GetCodeCoupon();
         }
 
     }
